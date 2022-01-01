f2842f6995d378dfe1f46968f8e2106c3c8a88fa
Nathan Kessler <nathant93@gmail.com>
2022-01-01 18:56:18 -0500

    Add TuiCSS constants

---
+++ tuicss-components/src/styles/Media.ts
@@ -0,0 +1,17 @@
+enum Media {
+  hideOnSmallOnly= "hide-on-small-only",
+	hideOnSmallAndDown = "hide-on-small-and-down",
+	hideOnMedAndDown = "hide-on-med-and-down",
+	hideOnMedAndUp = "hide-on-med-and-up",
+	hideOnMedOnly = "hide-on-med-only",
+	hideOnLargeOnly = "hide-on-large-only",
+	hideOnExtraLargeOnly = "hide-on-extra-large-only",
+	showOnExtraLarge = "show-on-extra-large",
+	showOnLarge = "show-on-large",
+	showOnMedium = "show-on-medium",
+	showOnSmall = "show-on-small",
+	showOnMediumAndUp = "show-on-medium-and-up",
+	showOnMediumAndDown = "show-on-medium-and-down",
+}
+
+export default Media
